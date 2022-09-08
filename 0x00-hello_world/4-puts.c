7cae8d3c184389a55b803f0b0bcbd47423f64e8c
uchimechike <uchimechike@gmail.com>
2022-09-08 15:22:08 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
-int main(void){
-	char *var = "Programming is like building a multilingual puzzle";
+int main(void)
+{
+	/*The C proram starts here ie the main function*/
+	char *var = "\"Programming is like building a multilingual puzzle";
+	
 	puts(var);
 	return (0);
 }
