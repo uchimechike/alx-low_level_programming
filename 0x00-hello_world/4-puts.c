fa89373099935fe0dab0432ff4bdf8d8e32e40f1
uchimechike <uchimechike@gmail.com>
2022-09-08 15:32:04 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*The C proram starts here ie the main function*/
 	char *var = "\"Programming is like building a multilingual puzzle";
-	
 	puts(var);
 	return (0);
 }
