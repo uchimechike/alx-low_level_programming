2dcafecc8a8f7385111ff7d59d2930685ab2e9ac
uchimechike <uchimechike@gmail.com>
2022-09-10 01:05:07 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *  *
+ *  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (!(i == 56 && j == 57))
+			{
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
