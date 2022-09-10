98f3ddb565d2980c8932af49a0b16fbf52d6e464
uchimechike <uchimechike@gmail.com>
2022-09-09 17:04:48 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *  *
+ *  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		} else
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
