881222335e2de6d024a072cdf0e5b45ae95a808e
uchimechike <uchimechike@gmail.com>
2022-09-11 05:29:58 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 	int i, j, k;
 
 	/*iterating loop for the three possible combinations of digits*/
-	for (i = 0; i < 8; i++)
+	for (i = 48; i < 56; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j < 57; j++)
 		{
-			for (k = j + 1; k < 10; k++)
+			for (k = j + 1; k < 58; k++)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (!(i == 7 && j == 8 && k == 9))
+				if (!(i == 55 && j == 56 && k == 57))
 				{
 					putchar(',');
 					putchar(' ');
@@ -28,6 +28,7 @@ int main(void)
 			}
 		}
 	}
+	putchar('\n');
 
 	return (0);
 }
