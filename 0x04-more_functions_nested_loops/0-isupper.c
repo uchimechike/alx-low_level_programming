48a346cd71b7baf080ecf1c3353f1f092bf9f118
uchimechike <uchimechike@gmail.com>
2022-09-15 14:25:14 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,16 @@
+/**
+ * _isupper - checks whether a character is uppercase or lowercase
+ *
+ * @ch: the character that is to be checked
+ *
+ * Return: an integer value. 0 or 1
+ */
+
 int _isupper(int ch)
 {
 	int val;
 
-	if(ch >= 65 && ch <= 90)
+	if (ch >= 65 && ch <= 90)
 	{
 		val = 1;
 	} else
