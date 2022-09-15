d1b65cf7ce7d4a40cabcca7246d980daebe37e10
uchimechike <uchimechike@gmail.com>
2022-09-15 05:53:36 -0700

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,11 @@
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -13,5 +21,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return largest;
+	return (largest);
 }
