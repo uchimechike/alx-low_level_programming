30cc2a3b383528ffc32fcec19381eae444d97083
uchimechike <uchimechike@gmail.com>
2022-09-15 05:50:32 -0700

    task2

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the largest number from three variables a, b and c
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+
+	largest = largest_number(a, b, c);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
