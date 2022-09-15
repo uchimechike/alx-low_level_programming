d9462e1e7245514b2e0636c0630e0f07f6a4bb05
uchimechike <uchimechike@gmail.com>
2022-09-15 14:39:13 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/**
+ * mul - funtion that multiplies two integers
+ *
+ * @a: first argument of the mul function
+ *
+ * @b: second argument of the mul function
+ *
+ * Return: returns a long int after multiplication
+ */
+
+int mul(long int a, long int b)
+{
+	long int result;
+
+	result = a * b;
+
+	return (result);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _isupper(int ch);
 int _isdigit(int ch);
+int mul(long int a, long int b);
