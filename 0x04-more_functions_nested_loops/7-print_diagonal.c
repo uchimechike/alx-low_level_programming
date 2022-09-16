cc9b7434ca58f3a8b54600a9b447c648f240b47e
uchimechike <uchimechike@gmail.com>
2022-09-15 18:33:17 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,23 +12,25 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	i = n;
-
-	while (n > 0)
+	if (n < 0 || n == 0)
 	{
-		if (n < 0 || n == 0)
-		{
-			break;
-		}
-		j = i - (n - 1);
-
-		while (j > 0)
-		{
-			_putchar(' ');
-			j--;
-		}
-		_putchar('\\');
 		_putchar('\n');
-		n--;
+	} else
+	{
+		i = n;
+
+		while (n > 0)
+		{
+			j = i - (n - 1);
+
+			while (j > 0)
+			{
+				_putchar(' ');
+				j--;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			n--;
+		}
 	}
 }
