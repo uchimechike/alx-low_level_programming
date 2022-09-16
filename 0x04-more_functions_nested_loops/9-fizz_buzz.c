fffce7ec5f3e450c3e6720c891d6afa27dd1a72e
uchimechike <uchimechike@gmail.com>
2022-09-16 07:26:25 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints numbers from 1 to 100 with certain conditions
+ *
+ * Return: Always 0 to indicate success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		} else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
