49cf5f2f3f5666659d49dc537fa417fe6d0e1b1a
uchimechike <uchimechike@gmail.com>
2022-09-16 05:40:29 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,28 +7,26 @@
  *
  * Return: no return value as return type of the funtion is void
  */
-
 void print_diagonal(int n)
 {
-	int a = 1;
+	int a = 0;
 	int b = 0;
 
 	if (n > 0)
 	{
-		while (a <= n)
+		while (a < n)
 		{
 			while (b < a)
 			{
 				_putchar(' ');
 				b++;
 			}
+			a++;
 			b = 0;
 			_putchar('\\');
 			_putchar('\n');
-			a++;
 		}
-	} else
-	{
-		_putchar('\n');
 	}
+	else
+	_putchar('\n');
 }
