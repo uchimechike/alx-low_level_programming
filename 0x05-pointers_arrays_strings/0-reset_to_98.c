3670a3c641820255ec40ff9d79edcb80fbac907d
uchimechike <uchimechike@gmail.com>
2022-09-19 10:42:21 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ *reset_to_98 - function that resets the value of the address it points to 
+ *
+ * @*n: parameter pointer passed to the function
+ *
+ *Return: no return value as type is void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
